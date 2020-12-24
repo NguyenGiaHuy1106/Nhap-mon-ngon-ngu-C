2bc41405b79527f340a3c4ba8e5e645a7d4aa26e
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2020-12-24 16:59:19 +0700

    Create tongn.c

---
+++ Bài tập 3/tongn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	long c;
+	c = 0;
+	a = 1;
+	//nhap gia cua n
+	printf("\n n:");
+	scanf ("%d", &b);
+	
+	for (a<=n)
+	  {  
+	  c=c+a;
+       a++;
+       }
+	printf("\n 1 + 2 + ... + %d: ", b, c);
+
+    return 0;
+}
