104e5fa9bf517a8b0cbf4fb3d106593d3b0a9bf4
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2020-12-24 14:13:43 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("Tu 1 den 10 : \n\n");
+	for (a=1; a <= 10; a+=1)
+	printf("%d\n", a);
+		int b;
+	printf("Tu 1 den 10 : \n\n");
+	for (b=1; b <= 10; b+=1)
+	printf("%d\t", b);
+    return 0;
+}
