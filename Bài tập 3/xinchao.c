74ba35ee9ee8f1508c813c35788c46c730ef3c0f
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2020-12-24 13:56:08 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int count;
+    
+    for (count = 1;  count <=9; count ++)
+    printf("\n \t \t Xin chao");
+    printf("\n\t\t Xin chao. \n");
+    return 0;
+}
