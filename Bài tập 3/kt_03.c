a7b5c27e4c877b5e0f1d5dc1788a90b8bf4cf4ca
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2021-01-05 22:11:50 +0700

    Create kt_03.c

---
+++ Bài tập 3/kt_03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(){
+int i;
+for(i = 1; i < 20; i+=2)
+//khong hien thi so 5, 11 va 15
+{if((i == 5) || (i == 11) || (i == 15))
+   continue;
+printf("%5d", i);
+}
+getch();
+return 0;
+}
