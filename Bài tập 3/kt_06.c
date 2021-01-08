8db210c3ed56705f07e1817d00a36f8e65a242c5
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2021-01-08 14:04:19 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 main(){
   int h;
-  printf("h=4\n");
+  
+  printf("h=");
+  scanf("%d", &h);
+  switch(h){
+ //mot hang 	
+case 1: printf("\t   *\n"); break;
+//hai hang
+case 2:
+printf("\t   *\n");
+printf("\t  ***\n");
+printf("h=");
+break;
+//ba hang
+case 3:
+printf("\t   *\n");
+printf("\t  ***\n");
+printf("\t *****\n");
+break;
+//bon hang
+case 4:
 printf("\t   *\n");
 printf("\t  ***\n");
 printf("\t *****\n");
 printf("\t*******\n");
+break;
+}
 getchar();
 return 0;
 }
