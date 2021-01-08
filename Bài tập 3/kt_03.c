65583dceae02cbc4847d4f18a9b32a14dbacc901
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2021-01-08 10:39:21 +0700

    Update kt_03.c

--- Bài tập 3/kt_03.c
+++ Bài tập 3/kt_03.c
@@ -1,3 +1,4 @@
+vong lap for
 #include <stdio.h>
 main(){
 int i;
@@ -7,6 +8,23 @@ for(i = 1; i < 20; i+=2)
    continue;
 printf("%5d", i);
 }
-getch();
+getchar();
+return 0;
+}
+
+vong lap while
+   #include <stdio.h>
+main(void){
+int i;
+i = 1;
+while(i < 19){
+	i+=2;
+//khong hien thi so 5, 11 va 15
+{if((i == 5) || (i == 11) || (i == 15))
+   continue;
+printf("%5d", i);
+}
+}
+getchar();
 return 0;
 }
