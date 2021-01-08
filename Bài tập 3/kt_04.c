7f747e04b77a931776d79865c928db249f63cd01
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2021-01-08 08:44:17 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -14,3 +14,30 @@ int main(){
     getchar ();
     return 0;
 }
+
+//khong dung for
+#include<stdio.h>
+int main(){
+    int n;
+    int m;
+    int s = 0;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+     //so thu nhat 
+     m = n % 10;
+     s += m;
+     n /= 10;
+    //so thu hai
+     m = n % 10;
+     s += m;
+     n /= 10;
+    //so thu ba
+     m = n % 10;
+     s += m;
+     n /= 10 ;
+     //hien thi ra ket qua cua 3 so
+     printf("%d", s);
+	getchar();
+	return 0;
+	}
+    
