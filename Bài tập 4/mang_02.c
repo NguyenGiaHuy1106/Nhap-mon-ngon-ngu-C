164e36e285acc8fc777b9bd4a7d6e52a9560f040
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2021-01-14 17:01:54 +0700

    Create mang_02.c

---
+++ Bài tập 4/mang_02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int n;
+	int a[n];
+	printf("Nhap n = ");
+	scanf("%d", &n);
+	for(n = 1; n < n; n++){
+	if(a[n]%2 == 0)
+	printf("%5d", n);
+    } 
+	getchar (); 
+     return 0;
+}
