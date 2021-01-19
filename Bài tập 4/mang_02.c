1ed2affbdc6dba9bcd8289ed4820d9a26f855f5a
NguyenGiaHuy1106 <72543345+NguyenGiaHuy1106@users.noreply.github.com>
2021-01-19 15:55:29 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
+  #include <stdio.h>
 int main(){
 	int n;
-	int a[n];
-	printf("Nhap n = ");
+	printf("Nhap n = ");//hien thi gia tri n
 	scanf("%d", &n);
-	for(n = 1; n < n; n++){
-	if(a[n]%2 == 0)
-	printf("%5d", n);
-    } 
-	getchar (); 
+  
+  int a[n];
+	for(int i = 0;i < n; i++){
+    printf("Nhap phan tu thu %d = ", i + 1);//hien thi thu tu phan tu trong mang a
+	scanf("%d", &a[i]);
+  }
+   printf("mang a: ");
+  for(int i = 0;i < n; i++){
+    printf(" %d ", a[i]);
+    
+  }
+  for(int i = 0;i < n; i++){
+  if (a[i] % 2 == 0) {
+      printf("\n%d la so chan\n", a[i]);// hien thi so chan trong mang a
+  }
+  }
      return 0;
 }
